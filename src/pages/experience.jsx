5858e22da2fa490c415ad8ba8e2df91e0eb28566
dardan8 <dardan.dara@gmail.com>
2023-02-19 14:55:17 +0100

    Updated button in experienece page

--- src/pages/experience.jsx
+++ src/pages/experience.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import styles from "../../src/styles/Experience.module.css";
 import Image from "next/image";
 import Button from "components/Button";
+import Link from "next/link";
 
 const Experience = () => {
   return (
     <div className={styles.experience_container}>
       <p>This page is under construction.</p>
       <p>Be back soon or contact me to get my CV.</p>
-      <Button type='button-primary' placeholder='Get in touch' />
+      <Link href='mailto: dardan.dara@gmail.coom'>
+        <Button type='button-primary' placeholder='Get in touch' />{" "}
+      </Link>
       <Image src='/images/Under_Construction.jpg' width={500} height={500} />
     </div>
   );
